343125020e5f7040b5e7b7535251beee79eef911
Piojo <lfmunozmejias@gmail.com>
2011-03-06 01:32:41 +0100

    Module with some utilitary functions, and its tests

---
+++ misc_utils.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import re
+
+def field_from_dictionary_list(field):
+    '''Decorates a function that returns a list of complex data
+    structures (tipically dictionaries), to iterate only over its
+    interesting values.'''
+    def wrap(f):
+        def extract_field_from_f(*args, **kw):
+            for i in f(*args, **kw):
+                yield i[field]
+        return extract_field_from_f
+    return wrap
+
+RX = re.compile('[,\s]+')
+
+def comma_separated_function_args(f):
+    def run(args):
+        l = RX.split(args)
+        return f(*l)
+    return run
