594da5337f235729448f6fb3695448bb3792727c
Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
2011-03-14 21:12:22 +0100

    Fix bugs on commit, function calls and really run commands

--- cdbwalk.py
+++ cdbwalk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- encode: utf-8 -*-
+# -*- encoding: utf-8 -*-
 '''Script that walks over a CDB Pan checkout, performs actions on
 templates that declare them, and commits any changes, if needed.
 
@@ -8,25 +8,57 @@ TODO:
 - Configure which actions map to which headers without modifying the
   code.
 
+- Make it generic enough to work with CDB and SCDB  
+
 Author: Luis Fernando Muñoz Mejías
 '''
 import sys
 import os
-from pan_file import pan_file
+from pan_file import pan_file, pan_exception
 from optparse import OptionParser
+import subprocess
+from egroup2pan import egroup_string_to_pan_variable as egroup2pan
+
+
+class cdb_error(Exception):
+    '''Exception for errors dealing with CDB'''
+    def __init__(self, descro, process):
+        self.msg = descro
+        self.process = process
+
+    def __str__(self):
+        lines = [self.msg, "\nStandard output:\n"]
+        lines.extend(l for l in self.process.stdout)
+        lines.append("\nStandard error:\n")
+        lines.extend(l for l in self.process.stderr)
+        return "".join(lines)
 
 def init_actions():
     '''Initialises the dictionary of actions'''
-    d = dict()
+    d = {"egroups" : egroup2pan}
+    return d
 
 def do_commit(*arg):
-    pass
+    '''Commits the modified templates to CDB'''
+    cdbcmd = ('update ' + ' '.join(arg) +
+              '; commit -c "Upgrade templates that come from external sources')
+    cdbcall = [ 'cdbop', '--server', 'cdbserv', '--cfgfile', '/dev/fd/0',
+                '--command',  cdbcmd]
+
+    p = subprocess.Popen(cdbcall, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    if not os.environ.has_key('CDB_USER'):
+        p.stdin.write('use-krb5=1\n')
+    p.stdin.close()
+    if p.wait() != 0:
+        raise cdb_error("Failed to commit to CDB", p)
 
 if __name__ == '__main__':
     o = OptionParser()
     o.add_option("-d", "--dir", dest="top",
                  help="Top level of the CDB checkout")
-    o.add_option("-e", "--extension", dest="extension"
+    o.add_option("-e", "--extension", dest="extension",
                  help="Extension of the Pan templates (defaults to %default)",
                  default=".tpl")
     (opts, args) = o.parse_args()
@@ -41,10 +73,11 @@ if __name__ == '__main__':
                 try:
                     fullpath = os.path.join(root, f)
                     p = pan_file(fullpath, **actions)
-                    p.processs()
+                    p.process()
                     if p.write_file():
                         changed.append(fullpath)
                 except pan_exception, e:
                     print e
-        do_commit(changed)
+    if changed:
+        do_commit(*changed)
     
