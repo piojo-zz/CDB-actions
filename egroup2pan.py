68be993f3c59ee29f7b90496d1575222a711a884
Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
2011-03-14 21:02:02 +0100

    Tests and bug fixes to the egroup2pan

--- egroup2pan.py
+++ egroup2pan.py
@@ -1,11 +1,14 @@
-'''Module querying LDAP for e-group combinations'''
+'''Module querying LDAP for e-group combinations and producing a Pan
+variable.'''
 import sys
 import os
 import re
 from pan_file import pan_exception, wrap_in_variable
 from egroup_filter_generator import egroup_querier
 
-def egroup_string_to_ldap_query(string):
+def egroup_string_to_pan_variable(string):
+    '''Returns a pan variable with the list of e-groups included (or
+    excluded) from the in the argument string.'''
     rx = re.compile("(\w+)=(\S+)")
     d = dict()
     var_name = []
@@ -16,9 +19,10 @@ def egroup_string_to_ldap_query(string):
         d[m.group(1)] = m.group(2).split(',')
         var_name.extend(d[m.group(1)])
 
-    var_name = ''.join(var_name)
+    var_name = '_'.join(var_name)
     @wrap_in_variable(var_name)
     def f():
-        egroup_querier(**d)
+        rs, members = egroup_querier(**d)
+        return [ x[1]['sAMAccountName'][0] for x in members]
 
     return f()
--- test.py
+++ test.py
@@ -5,3 +5,4 @@ from doctest import testfile
 testfile('tests/egroups.txt')
 testfile('tests/fields_from_dict.txt')
 testfile('tests/pan_file.txt')
+testfile('tests/egroup2pan.txt')
