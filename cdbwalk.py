fd2e36be15f276a739df8cd22aeef79340198fe5
Piojo <lfmunozmejias@gmail.com>
2011-04-06 02:05:06 +0200

    Add a verbose mode

--- cdbwalk.py
+++ cdbwalk.py
@@ -19,7 +19,6 @@ from optparse import OptionParser
 import subprocess
 from egroup2pan import egroup_string_to_pan_variable as egroup2pan
 
-
 class cdb_error(Exception):
     '''Exception for errors dealing with CDB'''
     def __init__(self, descro, process):
@@ -32,6 +31,14 @@ class cdb_error(Exception):
         lines.append("\nStandard error:\n")
         lines.extend(l for l in self.process.stderr)
         return "".join(lines)
+
+class log(object):
+    '''Log object for displaying information'''
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+    def __call__(self, *kwargs):
+        if self.verbose:
+            print ' '.join(kwargs)
 
 def init_actions():
     '''Initialises the dictionary of actions'''
@@ -63,8 +70,12 @@ if __name__ == '__main__':
                  default=".tpl")
     o.add_option("-c", "--commit", dest="commit",
                  help="Commit to CDB any changes",
-                 action="store_true") 
+                 action="store_true")
+    o.add_option("-v", "--verbose", dest="verbose",
+                 help="Verbose",
+                 action="store_true")
     (opts, args) = o.parse_args()
+    lg = log(opts.verbose)
     changed = []
     actions = init_actions()
     for root, dirs, files in os.walk(opts.top):
@@ -75,9 +86,11 @@ if __name__ == '__main__':
             if f.endswith(opts.extension):
                 try:
                     fullpath = os.path.join(root, f)
+                    lg("Processing template:", fullpath)
                     p = pan_file(fullpath, **actions)
                     p.process()
                     if p.write_file():
+                        lg("Modified:", fullpath)
                         changed.append(fullpath)
                 except pan_exception, e:
                     print e
